76f60cc4463f642feba6e02bdb7ed91a478c4753
Gilang Ramadhan <gilangr1440@gmail.com>
2023-03-14 09:59:15 +0700

    register page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "../components/Footer";
+
+const Register = () => {
+  return (
+    <div>
+      <div className="h-screen"></div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Register;
--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -3,12 +3,14 @@ import { useCookies } from "react-cookie";
 
 import Login from "../pages/Login";
 import Reserve from "../pages/Reserve";
+import Register from "../pages/Register";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/reservasi" element={<Reserve />} />
       </Routes>
     </BrowserRouter>

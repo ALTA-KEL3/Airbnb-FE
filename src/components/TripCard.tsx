0277fd9c543f2a5ce0952deb024e8c2c5ff81f78
Agung Cahya <agungcahya122@gmail.com>
2023-03-17 01:17:21 +0700

    feat: integrasi trip with local storage

--- src/components/TripCard.tsx
+++ src/components/TripCard.tsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { FC } from "react";
+import { useNavigate } from "react-router";
+
 import CustomInput from "./CustomInput";
 
-const TripCard = () => {
+interface TripProps {
+  id?: string;
+  name?: string;
+  checkIn?: string;
+  checkOut?: string;
+  price?: string;
+  day?: string;
+  total?: string;
+}
+
+const TripCard: FC<TripProps> = ({
+  id,
+  name,
+  checkIn,
+  checkOut,
+  price,
+  day,
+  total,
+}) => {
+  const navigate = useNavigate();
+
   return (
     <div className="card w-3/4 bg-base-100 shadow-md">
       <div className="card-body">
-        <h1 className="text-xl font-extrabold">Villa Premium A3</h1>
+        <h1 className="text-xl font-extrabold">{name}</h1>
         <div className="my-2 flex gap-4">
           <div className="w-1/4 space-y-2">
             <p>Check - In :</p>
-            <CustomInput id="input-startClass" type="date" placeholder="" />
+            <p>{checkIn}</p>
           </div>
           <div className="w-1/4 space-y-2">
             <p>Check - Out :</p>
-            <CustomInput id="input-startClass" type="date" placeholder="" />
+            <p>{checkOut}</p>
           </div>
         </div>
-        <p className="text-[20px] font-semibold">$ 100 x 2 night</p>
-        <p className="text-[24px] font-semibold">Total $200</p>
-        <button className="btn-sm btn my-3 w-52 bg-color3">Beri Penilaian</button>
+        <p className="text-[20px] font-semibold">$ 100 x {day} malam</p>
+        <p className="text-[24px] font-semibold">Total Rp.{total}</p>
+        <button
+          onClick={() => navigate("/feedback")}
+          className="btn-sm btn my-3 w-52 bg-color3"
+        >
+          Beri Penilaian
+        </button>
       </div>
     </div>
   );
